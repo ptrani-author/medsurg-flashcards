bbf7019d28bf597aa5ca897fbc207613a5df829b
ptrani-author <ptrani.author@gmail.com>
2025-04-03 21:01:03 +0200

    Update flashcards.js

--- flashcards.js
+++ flashcards.js
@@ -1,1 +1,15 @@
-const flashcards = [...flashcardsGroup1];
+const flashcards = [
+  ...flashcardsGroup1,
+  ...flashcardsGroup2,
+  ...flashcardsGroup3,
+  ...flashcardsGroup4,
+  ...flashcardsGroup5,
+  ...flashcardsGroup6,
+  ...flashcardsGroup7,
+  ...flashcardsGroup8,
+  ...flashcardsGroup9,
+  ...flashcardsGroup10,
+  ...flashcardsGroup11,
+  ...flashcardsGroup12,
+  ...flashcardsGroup13
+];
